d8c93cbe20417311b48ec93ff1c6f82ed6281029
Abdullah Muhammad Siam <abdullahms838@gmail.com>
2023-08-05 00:06:59 +0600

    firebase

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAf1U5ZXbYzTnxFKjffevBSkfi8VNEZgXc",
+    authDomain: "language-school-3ec51.firebaseapp.com",
+    projectId: "language-school-3ec51",
+    storageBucket: "language-school-3ec51.appspot.com",
+    messagingSenderId: "690568578785",
+    appId: "1:690568578785:web:e4a854a7564d117e4b99a0"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import Main from './layout/Main.jsx';
 import Home from './pages/Home/Home.jsx';
 import Login from './pages/login/Login.jsx';
 import Register from './pages/register/Register.jsx';
+import AuthProvider from './providers/AuthProvider.jsx';
 
 const router = createBrowserRouter([
   {
@@ -34,8 +35,10 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <div className='container mx-auto'>
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>,
+    <AuthProvider>
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>,
+    </AuthProvider>
   </div>
 )
